6eb997df2b1e09a7462810d339def8a736e51247
Nicole Chen <nschen01@gmail.com>
2024-01-14 15:38:26 -0800

    Add different users + theme

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,12 +1,49 @@
-import {ReactElement} from "react";
-import nicflixLogo from './images/nicflix-logo.png'
+import { ReactElement } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import nicflixLogo from "./images/nicflix-logo.png";
+import cat1 from "./images/cat1.jpg";
+import cat2 from "./images/cat2.jpg";
 
 const Landing = (): ReactElement => {
-    return (
-        <div>
-            <img src={nicflixLogo} alt="nicflix-logo"></img>
-        </div>
-    );
-}
+	return (
+		<Container
+			fluid
+			className="bg-dark text-light d-flex align-items-center justify-content-center vh-100"
+		>
+			<div className="main-content">
+				<Row>
+					<Col>
+						<div className="icon-container text-center">
+							<img src={nicflixLogo} alt="nicflix-logo" />
+						</div>
+					</Col>
+				</Row>
+				<Row>
+					<Col>
+						<p className="mt-2 text-center fs-5">Who's watching?</p>
+					</Col>
+				</Row>
+				<Row>
+					<Col className="text-center">
+						<img
+							src={cat1}
+							alt="cat-through-fence"
+							style={{ width: "175px", height: "175px" }}
+						/>
+						<p className="mt-2">Future Employers</p>
+					</Col>
+					<Col className="text-center">
+						<img
+							src={cat2}
+							alt="cat-sleeping"
+							style={{ width: "175px", height: "175px" }}
+						/>
+						<p className="mt-2">Friends & Fam</p>
+					</Col>
+				</Row>
+			</div>
+		</Container>
+	);
+};
 
 export default Landing;
