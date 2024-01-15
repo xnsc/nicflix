6460479b38cd52ddc5f14d4e303125fa9df8db07
Nicole Chen <nschen01@gmail.com>
2024-01-14 20:50:09 -0800

    Add /portfolio and /browse routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
 
 import Landing from "./components/Landing";
+import Portfolio from "./components/Portfolio";
+import Recommendations from "./components/Recommendations";
 
 const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-      </Routes>
-    </Router>  
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<Landing />} />
+				<Route path="/portfolio" element={<Portfolio />} />
+				<Route path="/browse" element={<Recommendations />} />
+			</Routes>
+		</Router>
+	);
 };
 
 export default App;
