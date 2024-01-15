874bce22a1654d0489720b6452f18cb7e51accfc
Nicole Chen <nschen01@gmail.com>
2024-01-14 20:53:36 -0800

    Initial commit

---
+++ src/components/Recommendations.tsx
@@ -0,0 +1,13 @@
+import { ReactElement } from "react";
+import { Container } from "react-bootstrap";
+
+const Recommendations = (): ReactElement => {
+	return (
+		<Container
+			fluid
+			className="bg-dark text-light d-flex align-items-center justify-content-center vh-100"
+		></Container>
+	);
+};
+
+export default Recommendations;
