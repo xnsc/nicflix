b277526472d3e5dae5ad0f2d232763039d3da496
Nicole Chen <nschen01@gmail.com>
2024-08-07 22:52:20 -0700

    Add main center card

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -6,7 +6,20 @@ const Portfolio = (): ReactElement => {
 		<Container
 			fluid
 			className="bg-dark text-light d-flex align-items-center justify-content-center vh-100"
-		></Container>
+		>
+			<div
+				className="rounded p-4 shadow"
+				style={{
+					backgroundColor: "#faf2ed",
+					maxWidth: "50vw",
+					maxHeight: "75vh",
+					width: "100%",
+					height: "100%",
+				}}
+			>
+				{/* Your content goes here */}
+			</div>
+		</Container>
 	);
 };
 
